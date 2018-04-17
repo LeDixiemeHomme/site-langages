dff39cd6c6e2e9749525c5d6038f4ec3e46e1ac5
Valle1 <benoit.valleea@hotmail.fr>
2018-04-17 23:44:03 +0200

    html et css

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+
+	<head>
+	
+	<title> Accueil principal </title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="style accueil.css">
+ 
+	</head>
+ 
+	<body>
+
+		<div class="photo1">
+
+			<p> <img src="pictures/all_language_web.png" width="300px"> </p>
+		
+		</div>
+		
+			<h1 class="titre1"> Informations utiles à propos de 4 langages de programmation </h1>
+			
+			<p class="paragraphe1"> Sur ce site vous il sera possible de se renseigner sur l'histoire et l'apprentissage des bases des langages suivants :<br> <span class="leslangages"> <strong> HTML, CSS, PHP et Javascript </strong> </span> </p>
+			<p class="paragraphe2" > En effet, chaque lien cliquable vous enverra vers une page qui contient une définition,<br> une photo du créateur du langage, une vidéo tutoriel,<br> mais aussi un pdf qui regroupe les informations principales à connaître pour débuter </p>
+
+			<ul class="liste1"> Liens vers les autres langages : <br>
+
+				<li><a href="HTML.html" target="blank"> Lien vers la page HTML </a></li>
+				<li><a href="CSS.html" target="blank"> Lien vers la page CSS </a></li>
+				<li><a href="PHP.html" target="blank"> Lien vers la page PHP </a></li>
+				<li><a href="Javascript.html" target="blank"> Lien vers la page Javascript </a></li>
+
+			</ul>
+			
+		<div class="photosenligne2">
+
+			<p> <img src="pictures/image_bulles.png" width="150px">
+			<img src="pictures/html-css1.png" width="170px">
+			<img src="pictures/image_bulles.png" width="150px"> </p>
+		
+		</div>
+		
+	</body>
+
+</html>
